4e7ff929ee3426cd28b8ce0ecd5278c591b2483f
MiravBhojani <95929083+MiravBhojani@users.noreply.github.com>
2024-01-22 11:59:26 +0300

    updates

--- Cash Sales Customer/Form1.Designer.vb
+++ Cash Sales Customer/Form1.Designer.vb
@@ -22,7 +22,29 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Label1 = New Label()
+        DataGridView1 = New DataGridView()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.FlatStyle = FlatStyle.Flat
+        Label1.Location = New Point(321, 9)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(140, 25)
+        Label1.TabIndex = 0
+        Label1.Text = "Cash Customers"
+        ' 
+        ' DataGridView1
+        ' 
+        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView1.Location = New Point(0, 62)
+        DataGridView1.Name = "DataGridView1"
+        DataGridView1.RowHeadersWidth = 62
+        DataGridView1.Size = New Size(801, 225)
+        DataGridView1.TabIndex = 1
         ' 
         ' Form1
         ' 
@@ -30,9 +52,16 @@ Partial Class Form1
         AutoScaleMode = AutoScaleMode.Font
         BackColor = Color.Cornsilk
         ClientSize = New Size(800, 450)
+        Controls.Add(DataGridView1)
+        Controls.Add(Label1)
         Name = "Form1"
         Text = "Cash Sales Customer"
+        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
         ResumeLayout(False)
+        PerformLayout()
     End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents DataGridView1 As DataGridView
 
 End Class
--- Cash Sales Customer/Form1.vb
+++ Cash Sales Customer/Form1.vb
@@ -1,5 +1,13 @@
-﻿Public Class Form1
+﻿Imports System.IO
+
+Public Class Form1
     Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
 
     End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+    Private Sub laodfiles()
+
 End Class
